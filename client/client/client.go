69f6a547c2b79135f6ea030a9e6971c9fcccc872
Marcin Konwiak <marcin2899@gmail.com>
2024-11-08 22:25:47 +0100

    feat: base data pooling

--- client/client/client.go
+++ client/client/client.go
@@ -1,15 +1,24 @@
 package client
 
+import (
+	"context"
+	"time"
+)
+
 type Client struct {
 	statsCollector *statsCollector
 }
 
 func NewClient() *Client {
 	return &Client{
-		statsCollector: newStatsCollector(1), // Todo: make interval configurable
+		// Todo: make interval configurable
+		statsCollector: newStatsCollector(time.Duration(1) * time.Second),
 	}
 }
 
 func (c *Client) Start() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	c.statsCollector.start(ctx)
 }
--- client/client/stats.go
+++ client/client/stats.go
@@ -2,24 +2,51 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
 	"time"
 )
 
+// - czas działania systemu,
+// - użycie CPU,
+// - użycie pamięci RAM,
+// - użycie miejsca na dysku,
+// - liczba działających procesów,
+// - dla każdego kontenera docker uruchomionego w systemie: użycie CPU, użycie pamięci RAM, liczba działających procesów.
+
 type statsData struct {
-	timestamp      int64
-	cpuPercent     float64
-	ramTotal       uint64
-	ramAvailable   uint64
-	ramUsed        uint64
-	ramUsedPercent float64
+	timestamp int64
+	cpu       statsCpu
+	ram       statsRam
+	os        statsOs
+}
+
+type statsCpu struct {
+	percent float64
+}
+
+type statsRam struct {
+	total     uint64
+	available uint64
+	used      uint64
+}
+
+type statsOs struct {
+	os              string
+	platform        string
+	platformVersion string
+	processes       uint64
 }
 
 type statsCollector struct {
-	interval int
+	interval time.Duration
 	data     chan []statsData
 }
 
-func newStatsCollector(interval int) *statsCollector {
+func newStatsCollector(interval time.Duration) *statsCollector {
 	return &statsCollector{
 		interval: interval,
 		data:     make(chan []statsData),
@@ -27,7 +54,7 @@ func newStatsCollector(interval int) *statsCollector {
 }
 
 func (s *statsCollector) start(ctx context.Context) {
-	ticker := time.NewTicker(time.Duration(s.interval) * time.Second)
+	ticker := time.NewTicker(s.interval)
 	defer ticker.Stop()
 
 	for {
@@ -40,14 +67,49 @@ func (s *statsCollector) start(ctx context.Context) {
 	}
 }
 
-func (s *statsCollector) collectStats() {
+func (s *statsCollector) collectStats() statsData {
+	d := statsData{}
+	d.cpu, _ = s.getCpuPercent()
+	d.ram, _ = s.getRamData()
+	d.os, _ = s.getOsData()
 
+	fmt.Printf("%+v\n", d)
+
+	return d
 }
 
-func (s *statsCollector) getCpuData() {
+func (s *statsCollector) getCpuPercent() (statsCpu, error) {
+	c, err := cpu.Percent(s.interval, false)
+	if len(c) != 1 {
+		return statsCpu{}, errors.New("invalid cpu cores amount")
+	}
 
+	return statsCpu{percent: c[0]}, err
 }
 
-func (s *statsCollector) getRamData() {
+func (s *statsCollector) getRamData() (statsRam, error) {
+	m, err := mem.VirtualMemory()
+	if err != nil {
+		return statsRam{}, err
+	}
 
+	return statsRam{
+		total:     m.Total,
+		available: m.Available,
+		used:      m.Used,
+	}, err
+}
+
+func (s *statsCollector) getOsData() (statsOs, error) {
+	o, err := host.Info()
+	if err != nil {
+		return statsOs{}, err
+	}
+
+	return statsOs{
+		os:              o.OS,
+		platform:        o.Platform,
+		platformVersion: o.PlatformVersion,
+		processes:       o.Procs,
+	}, err
 }
