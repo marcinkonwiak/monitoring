5e940c34e8e6a1952222af098a674e17ff6ab552
Michal Konwiak <michal.konwiak@smartive.app>
2024-11-16 14:15:53 +0100

    feature: add async creating 'hostdata' objs, ui changes

--- server/system_monitor/urls.py
+++ server/system_monitor/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", views.BaseLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    # path("", views.HomeView.as_view(), name="home"),
-    # path("instances/", views.HomeView.as_view(), name="instances"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("instances/", views.HomeView.as_view(), name="instances"),
 ]
