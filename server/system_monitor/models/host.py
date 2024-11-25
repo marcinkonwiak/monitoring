904d3fcf6ae74c5bdde493ceb5a9c50d06994473
Michal Konwiak <michalkonwiak1@gmail.com>
2024-11-25 22:32:59 +0100

    feature/server5 - refacto entity logic, minor changes

--- server/system_monitor/models/host.py
+++ server/system_monitor/models/host.py
@@ -5,28 +5,38 @@ from typing import Any
 from django.db import models
 
 
+class Host(models.Model):
+    host_id = models.CharField(max_length=255, unique=True, primary_key=True)
+    os = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_seen = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.host_id
+
+
 class HostStats(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    host = models.ForeignKey(Host, on_delete=models.CASCADE, related_name="stats")
     time = models.DateTimeField()
     cpu_percent = models.FloatField()
     ram_total = models.IntegerField()
     ram_available = models.IntegerField()
     ram_used = models.IntegerField()
-    host_id = models.CharField(max_length=255)
     os = models.CharField(max_length=255)
     platform = models.CharField(max_length=255)
     platform_version = models.CharField(max_length=255)
     processes = models.IntegerField()
 
     @classmethod
-    def from_proto_dict(cls, data: Any) -> "HostStats":
+    def from_proto_dict(cls, host: Host, data: Any) -> "HostStats":
         return cls(
+            host=host,
             time=datetime.fromtimestamp(data.timestamp),
             cpu_percent=data.cpu.percent,
             ram_total=data.ram.total,
             ram_available=data.ram.available,
             ram_used=data.ram.used,
-            host_id=data.os.hostID,
             os=data.os.os,
             platform=data.os.platform,
             platform_version=data.os.platformVersion,
